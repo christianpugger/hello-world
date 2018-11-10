24d488a59587a9de92087410c3d4e347b50e071b
Christian Bilato <christianpugger@gmail.com>
2018-11-10 20:17:35 +0100

    Update index

---
+++ index.js
@@ -0,0 +1,7 @@
+exports.printMsg = function() {
+  console.log("This is a message from the demo package");
+}
+
+var alexaPlusUnityClass = require('alexaplusunity');
+
+var alexaPlusUnity = alexaPlusUnityClass("<pub-c-bd427814-977f-46ad-89c1-551729f998f5>", "<sub-c-981df1ac-e4d2-11e8-868f-4a0ce4acae10>", true); //Third parameter enables verbose logging
